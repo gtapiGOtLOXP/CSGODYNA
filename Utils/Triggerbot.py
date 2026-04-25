43d1a3c60399954781a78cf80f34761ef27a4c52
gtapiGOtLOXP <232186334+gtapiGOtLOXP@users.noreply.github.com>
2026-04-25 14:57:17 +0200

    Update Triggerbot.py

--- Utils/Triggerbot.py
+++ Utils/Triggerbot.py
@@ -3,7 +3,6 @@ from Utils.Offsets import *
 from Utils.Utilities import is_pressed
 # Cleaned all of this file,
 # This file is working.
-# Last update 2022,Jan,10
 
 
 def shootTrigger(pm, CrossID, client, lTeam, CTeam, triggerkey):
